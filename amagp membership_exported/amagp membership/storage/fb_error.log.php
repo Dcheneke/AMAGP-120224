db1ba7ab7cda8c5bb2c46f6a2670f0de1967db90
Dcheneke <159797429+Dcheneke@users.noreply.github.com>
2024-02-12 19:47:33 +0200

    AMAGP - Feb 2024

---
+++ amagp membership_exported/amagp membership/storage/fb_error.log.php
@@ -0,0 +1,13 @@
+<?php echo 'Access denied.'; exit(); ?>
+Sun, 18 Mar 2018 07:44:09 +0000: Sending PHP mail message failed. (to, from, reply, subject)= [0] sanavyit@mweb.co.za   [1] membership@amagp.co.za   [2]    [3] Thank you for your appplication
+Sun, 18 Mar 2018 07:44:09 +0000: Sending PHP mail message failed. (to, from, reply, subject)= [0] membership@amagp.co.za   [1] amagpmembership@www.amagp.co.za   [2]    [3] AMAGP Membership Application Form
+Sun, 18 Mar 2018 07:44:09 +0000: Couldn't open or read: amagp membership.html
+Sun, 18 Mar 2018 07:44:09 +0000: Failed to parse HTML form.
+Sun, 18 Mar 2018 07:48:49 +0000: Sending PHP mail message failed. (to, from, reply, subject)= [0] sanavyit@mweb.co.za   [1] membership@amagp.co.za   [2]    [3] Thank you for your appplication
+Sun, 18 Mar 2018 07:48:49 +0000: Sending PHP mail message failed. (to, from, reply, subject)= [0] membership@amagp.co.za   [1] amagpmembership@www.amagp.co.za   [2]    [3] AMAGP Membership Application Form
+Sun, 18 Mar 2018 07:48:49 +0000: Couldn't open or read: amagp membership.html
+Sun, 18 Mar 2018 07:48:49 +0000: Failed to parse HTML form.
+Sun, 18 Mar 2018 11:23:50 +0000: Sending PHP mail message failed. (to, from, reply, subject)= [0] sanavyit@mweb.co.za   [1] membership@amagp.co.za   [2]    [3] Thank you for your appplication
+Sun, 18 Mar 2018 11:23:50 +0000: Sending PHP mail message failed. (to, from, reply, subject)= [0] membership@amagp.co.za   [1] amagpmembership@www.amagp.co.za   [2]    [3] AMAGP Membership Application Form
+Sun, 18 Mar 2018 11:23:50 +0000: Couldn't open or read: amagp membership.html
+Sun, 18 Mar 2018 11:23:50 +0000: Failed to parse HTML form.
